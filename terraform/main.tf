2753751ab16badb8234a0b7ea325fa5bd4071b99
Imran1221 <imranntelichalil@hotmail.de>
2022-11-21 18:12:01 +0100

    update gitignore and correction of main.tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,42 +1,41 @@
 terraform {
   required_providers {
-    azurem = {
-        source = "hashicorp/azurem"
+    azurerm = {
+        source = "hashicorp/azurerm"
         version = "~>3.0.2"
     }
   }
   required_version = ">=1.0.0"
 }
 
-provider "azurem"{
+provider "azurerm"{
     features {}
 }
 
-resource "azurem_ressource_group" "rg-devolsun" {
+resource "azurerm_resource_group" "rg-devolsun" {
   name = "rg-devolsun"
-  location = "europe"
+  location = "West Europe"
 }
 
-resource "azurem_service_plan" "app-service-plan" {
+resource "azurerm_service_plan" "app-service-plan" {
     name = "plan-dhbw"
-    ressource_group_name = azurem_ressource_group.rg-devolsun.name
-    location = azurem_ressource_group.rg-devolsun.location
+    resource_group_name = azurerm_resource_group.rg-devolsun.name
+    location = azurerm_resource_group.rg-devolsun.location
     sku_name = "F1"
-    os_type = "linux"
+    os_type = "Linux"
   
 }
 
-ressource "azurem_linux_web_app" "app"{
+resource "azurerm_linux_web_app" "app"{
     name = "app-dhbw-test123"
-    ressource_group_name = azurem_ressource_group.rg-devolsun.name
-    location = azurem_ressource_group.rg-devolsun.location
-    service_plan_id = azurem_service_plan.app-service-plan.id
-}
-
-site_config{
+    resource_group_name = azurerm_resource_group.rg-devolsun.name
+    location = azurerm_resource_group.rg-devolsun.location
+    service_plan_id = azurerm_service_plan.app-service-plan.id
+  site_config{
     always_on = false
     application_stack {
         docker_image = "hakcaliskan/devolsun"
         docker_image_tag = "latest"
     }
+  }
 }
